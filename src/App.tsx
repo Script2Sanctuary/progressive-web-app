3b4acd2bef717d7cbab025bcfffed9f73f0ef53c
royazizbarera <royazizbarera@gmail.com>
2024-08-26 19:18:39 +0700

    Add pwa

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +20,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>Counter: {count}</p>
+        <button onClick={increment}>Increase</button>
+        <button onClick={decrement}>Decrease</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +34,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
